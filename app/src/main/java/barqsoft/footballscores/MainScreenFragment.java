d3da9648404aff8c8f542077fa042c0d12fb91f7
Derek Morrison <DerekMorrison59@gmail.com>
2016-01-10 14:24:05 -0700

    Moved Day Name to be stored in the MainScreenFragment Moved method 'update_scores' into PagerFragment (it does not need to be run every time the user changes pages) Added local variables to store the date for this fragment and it's corresponding Day name

--- app/src/main/java/barqsoft/footballscores/MainScreenFragment.java
+++ app/src/main/java/barqsoft/footballscores/MainScreenFragment.java
@@ -1,6 +1,6 @@
 package barqsoft.footballscores;
 
-import android.content.Intent;
+import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,7 +13,8 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import barqsoft.footballscores.service.myFetchService;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -22,26 +23,32 @@ public class MainScreenFragment extends Fragment implements LoaderManager.Loader
 {
     public scoresAdapter mAdapter;
     public static final int SCORES_LOADER = 0;
-    private String[] fragmentdate = new String[1];
-    private int last_selected_item = -1;
+    long mDateInMilliseconds;
+    String mDayName;
+    private String[] mFragmentdate = new String[1];
+    //private int last_selected_item = -1;
 
     public MainScreenFragment()
     {
     }
 
-    private void update_scores()
-    {
-        Intent service_start = new Intent(getActivity(), myFetchService.class);
-        getActivity().startService(service_start);
+    public void setFragmentDate(Context context, long date){
+        mDateInMilliseconds = date;
+        Date fragmentdate = new Date(date);
+
+        // TODO fix date format for locale
+        SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
+
+        mFragmentdate[0] = mformat.format(fragmentdate);
+
+        mDayName = Utilies.getDayName(context, date);
     }
-    public void setFragmentDate(String date)
-    {
-        fragmentdate[0] = date;
-    }
+    public long getFragmentDate() { return mDateInMilliseconds; }
+    public String getDayName() { return mDayName; }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              final Bundle savedInstanceState) {
-        update_scores();
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         final ListView score_list = (ListView) rootView.findViewById(R.id.scores_list);
         mAdapter = new scoresAdapter(getActivity(),null,0);
@@ -66,7 +73,7 @@ public class MainScreenFragment extends Fragment implements LoaderManager.Loader
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle)
     {
         return new CursorLoader(getActivity(),DatabaseContract.scores_table.buildScoreWithDate(),
-                null,null,fragmentdate,null);
+                null, null, mFragmentdate, null);
     }
 
     @Override
@@ -82,6 +89,7 @@ public class MainScreenFragment extends Fragment implements LoaderManager.Loader
         }
         */
 
+/*
         int i = 0;
         cursor.moveToFirst();
         while (!cursor.isAfterLast())
@@ -89,6 +97,7 @@ public class MainScreenFragment extends Fragment implements LoaderManager.Loader
             i++;
             cursor.moveToNext();
         }
+*/
         //Log.v(FetchScoreTask.LOG_TAG,"Loader query: " + String.valueOf(i));
         mAdapter.swapCursor(cursor);
         //mAdapter.notifyDataSetChanged();
