e8fcae37ce6d7fd136191d5c6094eef40bca0623
Derek Morrison <DerekMorrison59@gmail.com>
2016-01-10 13:40:10 -0700

    Moved the function 'getDayName' into Utilites Changed the default team crest to a football icon Added 4 more team crests to the list of known drawables Moved many hard coded strings into the strings.xml file to support translation

--- app/src/main/java/barqsoft/footballscores/Utilies.java
+++ app/src/main/java/barqsoft/footballscores/Utilies.java
@@ -1,5 +1,10 @@
 package barqsoft.footballscores;
 
+import android.content.Context;
+import android.text.format.Time;
+
+import java.text.SimpleDateFormat;
+
 /**
  * Created by yehya khaled on 3/3/2015.
  */
@@ -10,7 +15,8 @@ public class Utilies
     public static final int CHAMPIONS_LEAGUE = 362;
     public static final int PRIMERA_DIVISION = 358;
     public static final int BUNDESLIGA = 351;
-    public static String getLeague(int league_num)
+
+    public static String getLeague(Context context, int league_num)
     {
         switch (league_num)
         {
@@ -19,37 +25,37 @@ public class Utilies
             case CHAMPIONS_LEAGUE : return "UEFA Champions League";
             case PRIMERA_DIVISION : return "Primera Division";
             case BUNDESLIGA : return "Bundesliga";
-            default: return "Not known League Please report";
+            default: return context.getString(R.string.unknown_league_error_msg);
         }
     }
-    public static String getMatchDay(int match_day,int league_num)
+    public static String getMatchDay(Context context, int match_day,int league_num)
     {
         if(league_num == CHAMPIONS_LEAGUE)
         {
             if (match_day <= 6)
             {
-                return "Group Stages, Matchday : 6";
+                return context.getString(R.string.group_stages_6);
             }
             else if(match_day == 7 || match_day == 8)
             {
-                return "First Knockout round";
+                return context.getString(R.string.first_knockout_round);
             }
             else if(match_day == 9 || match_day == 10)
             {
-                return "QuarterFinal";
+                return context.getString(R.string.quarter_final);
             }
             else if(match_day == 11 || match_day == 12)
             {
-                return "SemiFinal";
+                return context.getString(R.string.semi_final);
             }
             else
             {
-                return "Final";
+                return context.getString(R.string.final_text);
             }
         }
         else
         {
-            return "Matchday : " + String.valueOf(match_day);
+            return context.getString(R.string.matchday) + String.valueOf(match_day);
         }
     }
 
@@ -69,8 +75,9 @@ public class Utilies
     {
         if (teamname==null){return R.drawable.no_icon;}
         switch (teamname)
-        { //This is the set of icons that are currently in the app. Feel free to find and add more
-            //as you go.
+        {   //This is the set of icons that are currently in the app. Feel free to find and add more as you go.
+
+            // it is ok to leave these strings here because they are only used once and they do not change by locale or language
             case "Arsenal London FC" : return R.drawable.arsenal;
             case "Manchester United FC" : return R.drawable.manchester_united;
             case "Swansea City" : return R.drawable.swansea_city_afc;
@@ -81,7 +88,53 @@ public class Utilies
             case "West Bromwich Albion" : return R.drawable.west_bromwich_albion_hd_logo;
             case "Sunderland AFC" : return R.drawable.sunderland;
             case "Stoke City FC" : return R.drawable.stoke_city;
-            default: return R.drawable.no_icon;
+            case "Udinese Calcio" : return R.drawable.udinese_calcio;
+            case "Atalanta BC" : return R.drawable.atalanta;
+            case "AS Roma" : return R.drawable.as_roma;
+            case "FC Barcelona" : return R.drawable.barcelona_fc;
+
+            // TODO Add more Team Crests to the resource folders
+            // Atalanta BC
+            // AC Chievo Verona
+            // Juventus Turin
+            // Hellas Verona FC
+            // SS Lazio
+            // Carpi FC
+            // AC Milan
+            // Bologna FC
+
+            //default: return R.drawable.no_icon;
+            // changed to make the display more friendly
+            default: return R.drawable.soccerball;
         }
     }
+
+    // converts a date (in milliseconds) into a user friendly string like 'Yesterday', 'Tuesday', etc.
+    public static String getDayName(Context context, long dateInMillis) {
+        // If the date is today, yesterday or tomorrow then
+        // return the localized version of "Today" (etc). instead of the actual day name.
+
+        Time t = new Time();
+        t.setToNow();
+        int julianDay = Time.getJulianDay(dateInMillis, t.gmtoff);
+        int currentJulianDay = Time.getJulianDay(System.currentTimeMillis(), t.gmtoff);
+        if (julianDay == currentJulianDay) {
+            return context.getString(R.string.today);
+        } else if ( julianDay == currentJulianDay +1 ) {
+            return context.getString(R.string.tomorrow);
+        }
+        else if ( julianDay == currentJulianDay -1)
+        {
+            return context.getString(R.string.yesterday);
+        }
+        else
+        {
+            Time time = new Time();
+            time.setToNow();
+            // Otherwise, the format is just the day of the week (e.g "Wednesday".
+            SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+            return dayFormat.format(dateInMillis);
+        }
+    }
+
 }
