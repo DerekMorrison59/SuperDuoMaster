77fa0ebb21bd054a2584bece690cc2d1281a0436
Derek Morrison <DerekMorrison59@gmail.com>
2016-01-10 14:16:59 -0700

    Moved the function 'getDayName' into Utilites Moved Day Name to be stored in the MainScreenFragment Moved method 'update_scores' into PagerFragment (it does not need to be run every time the user changes pages) Increased the font size for the PagerTabStrip to make it more readable Cleaned up some constants and variable names

--- app/src/main/java/barqsoft/footballscores/PagerFragment.java
+++ app/src/main/java/barqsoft/footballscores/PagerFragment.java
@@ -1,52 +1,68 @@
 package barqsoft.footballscores;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
-import android.text.format.Time;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import barqsoft.footballscores.service.myFetchService;
 
 /**
  * Created by yehya khaled on 2/27/2015.
  */
 public class PagerFragment extends Fragment
 {
+    private static final long MILLI_DAY = 86400000;
+    private static final int PAST_DAY_OFFSET = 2;
     public static final int NUM_PAGES = 5;
     public ViewPager mPagerHandler;
     private myPageAdapter mPagerAdapter;
-    private MainScreenFragment[] viewFragments = new MainScreenFragment[5];
+    private MainScreenFragment[] mViewFragments = new MainScreenFragment[NUM_PAGES];  // replace 5 with constant NUM_PAGES
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View rootView = inflater.inflate(R.layout.pager_fragment, container, false);
         mPagerHandler = (ViewPager) rootView.findViewById(R.id.pager);
         mPagerAdapter = new myPageAdapter(getChildFragmentManager());
-        for (int i = 0;i < NUM_PAGES;i++)
-        {
-            Date fragmentdate = new Date(System.currentTimeMillis()+((i-2)*86400000));
-            SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
-            viewFragments[i] = new MainScreenFragment();
-            viewFragments[i].setFragmentDate(mformat.format(fragmentdate));
-        }
         mPagerHandler.setAdapter(mPagerAdapter);
         mPagerHandler.setCurrentItem(MainActivity.current_fragment);
+        for (int i = 0; i < NUM_PAGES; i++)
+        {
+            mViewFragments[i] = new MainScreenFragment();
+            mViewFragments[i].setFragmentDate(getActivity(), System.currentTimeMillis() + ((i - PAST_DAY_OFFSET) * MILLI_DAY));
+        }
+
+        // make the tab titles big enough to read easily
+        PagerTabStrip pts = (PagerTabStrip)rootView.findViewById(R.id.pager_header);
+        pts.setTextSize(TypedValue.COMPLEX_UNIT_PT, 12);
+
+        // trigger the service to fetch the scores
+        update_scores();
+
         return rootView;
     }
+
+    private void update_scores()
+    {
+        Intent service_start = new Intent(getActivity(), myFetchService.class);
+        getActivity().startService(service_start);
+    }
+
     private class myPageAdapter extends FragmentStatePagerAdapter
     {
         @Override
         public Fragment getItem(int i)
         {
-            return viewFragments[i];
+            return mViewFragments[i];
         }
 
         @Override
@@ -59,37 +75,13 @@ public class PagerFragment extends Fragment
         {
             super(fm);
         }
+
         // Returns the page title for the top indicator
         @Override
         public CharSequence getPageTitle(int position)
         {
-            return getDayName(getActivity(),System.currentTimeMillis()+((position-2)*86400000));
-        }
-        public String getDayName(Context context, long dateInMillis) {
-            // If the date is today, return the localized version of "Today" instead of the actual
-            // day name.
-
-            Time t = new Time();
-            t.setToNow();
-            int julianDay = Time.getJulianDay(dateInMillis, t.gmtoff);
-            int currentJulianDay = Time.getJulianDay(System.currentTimeMillis(), t.gmtoff);
-            if (julianDay == currentJulianDay) {
-                return context.getString(R.string.today);
-            } else if ( julianDay == currentJulianDay +1 ) {
-                return context.getString(R.string.tomorrow);
-            }
-             else if ( julianDay == currentJulianDay -1)
-            {
-                return context.getString(R.string.yesterday);
-            }
-            else
-            {
-                Time time = new Time();
-                time.setToNow();
-                // Otherwise, the format is just the day of the week (e.g "Wednesday".
-                SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
-                return dayFormat.format(dateInMillis);
-            }
+            // use the date that was assigned at start-up, not one based on the current time
+            return mViewFragments[position].getDayName();
         }
     }
 }
