1db87b9031ab514e2b8fff70670635e259740137
Derek Morrison <DerekMorrison59@gmail.com>
2016-02-06 21:05:54 -0700

    Added all the league numbers as strings

--- app/src/main/java/barqsoft/footballscores/DatabaseContract.java
+++ app/src/main/java/barqsoft/footballscores/DatabaseContract.java
@@ -9,6 +9,21 @@ import android.provider.BaseColumns;
  */
 public class DatabaseContract
 {
+    public interface Leagues {
+        String BUNDESLIGA1 = "394";
+        String BUNDESLIGA2 = "395";
+        String LIGUE1 = "396";
+        String LIGUE2 = "397";
+        String PREMIER_LEAGUE = "398";
+        String PRIMERA_DIVISION = "399";
+        String SEGUNDA_DIVISION = "400";
+        String SERIE_A = "401";
+        String PRIMERA_LIGA = "402";
+        String BUNDESLIGA3 = "403";
+        String EREDIVISIE = "404";
+        String CHAMPIONS_LEAGUE = "362";
+    }
+
     public static final String SCORES_TABLE = "scores_table";
     public static final class scores_table implements BaseColumns
     {
